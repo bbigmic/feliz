dbcf44bcd4b78e3cda84fe9e5a5722f96a5a9a42
bbigmic <122184799+bbigmic@users.noreply.github.com>
2025-07-24 20:25:24 +0200

    add migrations folder

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    // appDir: true, // usunięte, bo domyślne w Next.js 14
   },
   images: {
     domains: ['localhost', 'via.placeholder.com'],
