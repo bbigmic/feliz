30998b16e52c45bd4c514c66001d074c2c3b5993
bbigmic <122184799+bbigmic@users.noreply.github.com>
2025-07-24 19:47:30 +0200

    first commit 2

--- .next/static/webpack/webpack.11f5ca81c485eca2.hot-update.js
+++ .next/static/webpack/webpack.11f5ca81c485eca2.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9318512a25846119"; }
+/******/ 	__webpack_require__.h = function() { return "1a411534391221cf"; }
 /******/ }();
 /******/ 
 /******/ }
