8045d00cb01cd210aa1ecfe125a7669fc6463b75
bbigmic <122184799+bbigmic@users.noreply.github.com>
2025-07-27 22:11:46 +0200

    api debug prisma fix failed migration

---
+++ .next/static/chunks/app/about/page-fce4c045f7da4995.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301,818],{2472:function(e,n,s){Promise.resolve().then(s.bind(s,8562)),Promise.resolve().then(s.bind(s,3909))}},function(e){e.O(0,[527,448,707,971,117,744],function(){return e(e.s=2472)}),_N_E=e.O()}]);
